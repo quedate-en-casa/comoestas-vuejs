01eac4c5cec3c0addf21b02d361260eedd8582f1
Jeferson Agudelo <jeferson.agudelo@karibu.cl>
2020-08-25 15:22:13 -0400

    [CHO]: Analitycs

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import '../conf/analitycs/tagmanager.js'
 import  '@fortawesome/fontawesome-free/css/all.min.css'
 import './../node_modules/bulma/css/bulma.css';
 
